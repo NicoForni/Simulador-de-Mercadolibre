afdba2fefc8772a887e96d10f0f0234a18ebb07b
nico <nico.forni@hotmail.com>
2022-04-06 00:04:47 -0300

    acomodamiento

--- src/Components/ItemSearch/ItemSearch.js
+++ src/Components/ItemSearch/ItemSearch.js
@@ -3,16 +3,16 @@ import "./ItemSearch.css";
 const ItemSearch = () => {
     
     
-    return(<>
+    return(
+        <>
             <div className="container-title">Electronica - otra cosa</div>                
-            <div className="container-items">
-                <div className="container-item">                                     
-                    <img className="container-image" src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2017/06/playa-sol.jpg?fit=960%2C638&ssl=1" alt="item" ></img>
-                    <div className="container-description"><h2>$1999</h2><br/>descripcion larga del articulo</div>
-                    <div className="container-place">Lugar</div>
-                </div>
-            </div>
-            </>
+            <div className="container-items">                                                     
+                <img className="container-image" src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2017/06/playa-sol.jpg?fit=960%2C638&ssl=1" alt="item" ></img>
+                <div className="container-description"><h2>$1999</h2><br/>descripcion larga del articulo</div>                
+                <div className="container-place">Lugar</div>  
+                               
+            </div>                        
+        </>
     )
 }
 
