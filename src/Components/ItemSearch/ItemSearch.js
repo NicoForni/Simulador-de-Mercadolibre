d918f04eac378a19641cdc0399bbac1a0c9e30e5
nico <nico.forni@hotmail.com>
2022-04-05 21:51:46 -0300

    ver

--- src/Components/ItemSearch/ItemSearch.js
+++ src/Components/ItemSearch/ItemSearch.js
@@ -4,13 +4,12 @@ const ItemSearch = () => {
     
     
     return(<>
-            <div className="container-title">
-                Electronica - otra cosa
-            </div>                
+            <div className="container-title">Electronica - otra cosa</div>                
             <div className="container-items">
-                <div className="container-image">                    
-                    <img src="https://maxihogar.vtexassets.com/arquivos/ids/162769/plancha-atma-maxihogar-seco.jpg?v=637484886431530000" alt="item" width="200px" height="200px" ></img>
-                    <h2>$1999</h2>
+                <div className="container-item">                                     
+                    <img className="container-image" src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2017/06/playa-sol.jpg?fit=960%2C638&ssl=1" alt="item" ></img>
+                    <div className="container-description"><h2>$1999</h2><br/>descripcion larga del articulo</div>
+                    <div className="container-place">Lugar</div>
                 </div>
             </div>
             </>
